479bbdabc9683ee0b3f3f56585f6c0cc1c4c7fa3
Beaver2323 <1305321851@qq.com>
2024-03-22 17:24:34 +0800

    2024/3/16/13:54

--- main.py
+++ main.py
@@ -33,11 +33,11 @@ def main():
     device = set_device.set_device()
     print('使用的计算硬件是', device)
     ##载入图像分类数据集
-    train_dataset,val_dataset,test_dataset,class_num = data_load.data_load('little_slipt_dataset')
+    train_dataset,val_dataset,test_dataset,class_num = data_load.data_load('slipt_dataset')
     ##创建映射索引
     generate_index.generate_index(train_dataset)
 
-    BATCH_SIZE = 8
+    BATCH_SIZE = 18
     train_loader = DataLoader(train_dataset,batch_size=BATCH_SIZE,shuffle=True,num_workers=4)
     val_loader = DataLoader(val_dataset,batch_size=BATCH_SIZE,shuffle=True,num_workers=4)
     test_loader = DataLoader(test_dataset,batch_size=BATCH_SIZE,shuffle=True,num_workers=4)
@@ -46,7 +46,7 @@ def main():
     weights = torch.load('torch/hub/checkpoints/vgg16-397923af.pth')
     vgg16.load_state_dict(weights)##加载预训练权重
     vgg16.classifier[6] = nn.Linear(vgg16.classifier[6].in_features, class_num)##替换全连接层
-    optimizer = optim.Adam(vgg16.classifier[6].parameters())##只训练最后一层
+    optimizer = optim.Adam(vgg16.classifier[6].parameters(),lr=0.001)##只训练最后一层
     ##训练设置
     vgg16 = vgg16.to(device)
     # 交叉熵损失函数
@@ -54,7 +54,7 @@ def main():
     # 训练轮次 Epoch
     EPOCHS = 20
     ##学习率下降策略
-    lr_scheduler_1 = lr_scheduler.StepLR(optimizer, step_size=5, gamma=0.5)
+    lr_scheduler_1 = lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.5)
     ##训练前设置
     epoch = 0
     batch_idx = 0
@@ -63,7 +63,8 @@ def main():
     log_train,tab_train_log = init_log.init_train_log(train_loader,device,vgg16,criterion,optimizer,epoch,batch_idx)
     log_val, tab_val_log = init_log.init_val_log(val_loader,device,vgg16,criterion,epoch)
     ##训练可视化
-    wandb.init(project='VGG+', name='248a(base_test_2)')
+    wandb.init(project='VGG++', name='25a_2020(base)')
+    print(' ')
     ##循环训练
     for epoch in range(1, EPOCHS + 1):
 
@@ -86,19 +87,20 @@ def main():
         # 保存最新的最佳模型文件
         if log_val['val_accuracy'] > best_val_accuracy:
             # 删除旧的最佳模型文件(如有)
-            old_best_checkpoint_path = 'torch/hub/checkpoints/best-{:.4f}.pth'.format(best_val_accuracy)
+            old_best_checkpoint_path = 'torch/hub/checkpoints/best-25a-2020-{:.4f}.pth'.format(best_val_accuracy)
             if os.path.exists(old_best_checkpoint_path):
                 os.remove(old_best_checkpoint_path)
             # 保存新的最佳模型文件
             best_val_accuracy = log_val['val_accuracy']
-            new_best_checkpoint_path = 'torch/hub/checkpoints/best-{:.4f}.pth'.format(log_val['val_accuracy'])
+            new_best_checkpoint_path = 'torch/hub/checkpoints/best-25a-2020-{:.4f}.pth'.format(log_val['val_accuracy'])
             torch.save(vgg16, new_best_checkpoint_path)
-            print('保存新的最佳模型', 'checkpoint/best-{:.4f}.pth'.format(best_val_accuracy))
-            # best_val_accuracy = log_test['val_accuracy']
+            print('保存新的最佳模型', 'checkpoint/best-25a-2020-{:.4f}.pth'.format(best_val_accuracy))
 
 
-    tab_train_log.to_csv('log/训练日志248a.csv', index=False)
-    tab_val_log.to_csv('log/验证日志248a.csv', index=False)
+    tab_train_log.to_csv('log/训练日志25a-2020.csv', index=False)
+    tab_val_log.to_csv('log/验证日志25a-2020.csv', index=False)
+    # 载入最佳模型作为当前模型
+    model = torch.load('checkpoint/best-25a-2020-{:.4f}.pth'.format(best_val_accuracy))
 
 if __name__ == '__main__':
     main()
